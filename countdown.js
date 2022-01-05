f41c982cb1d8d8869c5c140ac56817e5e04c4a80
ThanhHoa03 <hoal94623@gmail.com>
2022-01-05 21:15:51 +0700

    upload

--- countdown.js
+++ countdown.js
@@ -56,3 +56,18 @@ function showForm() {
         showLogin.style.display = "none";
     }
 }
+
+function showPass() {
+    var x = document.getElementById("myPass");
+    var y = document.getElementById("switch-icon");
+    if (x.type == "password") {
+        x.type = "text";
+    } else {
+        x.type = "password";
+    }
+    if (y.classList == "fas fa-eye-slash") {
+        y.classList = "fas fa-eye";
+    } else {
+        y.classList = "fas fa-eye-slash";
+    }
+}
